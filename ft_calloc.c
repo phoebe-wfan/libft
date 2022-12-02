68e97597bc017ec78222151c605548face203fc0
Wei FAN <wfan@e3r6p2.clusters.42paris.fr>
2022-12-02 16:22:40 +0100

    leak

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,17 @@
 /*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 16:55:07 by wfan              #+#    #+#             */
-/*   Updated: 2022/11/25 21:44:51 by wfan             ###   ########.fr       */
+/*   Updated: 2022/12/02 16:12:41 by wfan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdint.h>
 /*void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 	char	*tmp;
-	
+
 	tmp = (char *)s;
 	i = 0;
 	while (tmp && n != 0)
@@ -26,13 +26,18 @@
 		i++;
 	}
 }*/
+
 void	*ft_calloc(size_t nmemb, size_t size)
-{	
+{
 	void	*ptr;
+	size_t	max;
 
 	if (nmemb == 0 || size == 0)
 		return (malloc(0));
-	ptr = malloc(nmemb * size);
+	max = SIZE_MAX / nmemb;
+	if (size > max)
+		return (NULL);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 	ft_bzero(ptr, size * nmemb);
