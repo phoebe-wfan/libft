a2ac7265b4d2b8d33ef50c36a828291a16027d85
Wei FAN <wfan@e2r3p6.clusters.42paris.fr>
2022-12-09 15:10:08 +0100

    leak substr // makefile gcc->cc

--- ft_substr.c
+++ ft_substr.c
@@ -5,13 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/13 17:50:54 by wfan              #+#    #+#             */
-/*   Updated: 2022/11/27 18:16:06 by wfan             ###   ########.fr       */
+/*   Created: 2022/12/09 14:52:12 by wfan              #+#    #+#             */
+/*   Updated: 2022/12/09 15:09:54 by wfan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdint.h>
 /*size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -20,25 +20,97 @@
 	while (s[i] != '\0')
 		i++;
 	return (i);
-}*/
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str_cpy;
+	int		i;
+	size_t	len;
+
+	len = 0;
+	if (s)
+		len = ft_strlen(s);
+	str_cpy = malloc(len + 1);
+	if (!str_cpy)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str_cpy[i] = s[i];
+		i++;
+	}
+	str_cpy[i] = '\0';
+	return (str_cpy);
+}
+
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
-	char	*str_sub;
-	char	*str;
+	char	*tmp;
+
+	tmp = (char *)s;
+	i = 0;
+	while (tmp && n != 0)
+	{
+		tmp[i] = 0;
+		n--;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	max;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	max = SIZE_MAX / nmemb;
+	if (size > max)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size * nmemb);
+	return (ptr);
+}*/
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	size_t			l;
+	unsigned int	i;
+	size_t			j;
 
 	if (!s)
 		return (NULL);
-	str = (char *)s;
-	str_sub = malloc(len + 1);
-	if (!str_sub)
+	l = ft_strlen(s);
+	if (l < start)
+		return (ft_strdup("\0"));
+	if (l < len)
+		sub = (char *)ft_calloc(l + 1, sizeof(char));
+	else
+		sub = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!sub)
 		return (NULL);
 	i = 0;
-	while (str[start + i] && i < len && start < ft_strlen((const char *)str))
+	j = 0;
+	while (s[i])
 	{
-		str_sub[i] = str[start + i];
+		if (start <= i && j < len)
+			sub[j++] = s[i];
 		i++;
 	}
-	str_sub[i] = '\0';
-	return (str_sub);
+	return (sub);
 }
+/*
+#include <stdio.h>
+int	main ()
+{
+	char *s;
+	s = ft_substr("1", 420000, 4200000);
+	printf("%s\n", s);
+	free (s);
+	return (0);
+}*/
